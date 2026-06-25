7efc4a5db67af65eaf859f3dd2c69268a0c1f95a
LocallyCH <chenhaoli0906@gmail.com>
2026-06-25 22:07:47 +0800

    name adjusted

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -43,12 +43,12 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.webp",
+			"assets/images/DesktopWallpaper/desktop1.avif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.webp",
+			"assets/images/MobileWallpaper/mobile1.avif",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组）
