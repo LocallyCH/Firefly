852fb3988c371ff86cb46316500bf83199683622
LocallyCH <chenhaoli0906@gmail.com>
2026-06-25 21:40:00 +0800

    wallpaper config

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -44,21 +44,11 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
 			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
 			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组）
@@ -81,17 +71,24 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Take a stroll down the lane...",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"We shall not cease from exploration",
+				"And the end of all our exploring",
+				"Will be to arrive where we started", 
+				"And know the place for the first time.",
+				"Through the unknown, remembered gate",
+				"When the last of earth left to discover",
+				"Is that which was the beginning;",
+				"At the source of the longest river",
+				"The voice of the hidden waterfall",
+				"And the children in the apple-tree",
+				"Not known, because not looked for",
+				"But heard, half heard, in the stillness",
+				"Between the two waves of the sea.",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
