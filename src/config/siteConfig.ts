ef6c63358354344083fc8948e7f1325fd9eebf19
LocallyCH <chenhaoli0906@gmail.com>
2026-06-25 22:53:20 +0800

    update favicon and navpic

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -56,7 +56,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/favicon.ico",
+			src: "/favicon/myfavicon.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
@@ -74,7 +74,7 @@ export const siteConfig: SiteConfig = {
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
 			type: "image",
-			value: "assets/images/firefly.png",
+			value: "assets/images/hazylane.png",
 			alt: "🍀",
 		},
 		// 导航栏标题
